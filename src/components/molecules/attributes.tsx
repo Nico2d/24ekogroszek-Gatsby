196e5ecc788b19bb982f3b580ab79c72334e0ed1
Nico <nico30d@gmail.com>
2021-03-09 10:04:18 +0100

    refactor: separte attributes

---
+++ src/components/molecules/attributes.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styled from "styled-components";
+import { device } from "../../styles/breakpoints";
+
+const calc = require("../../assets/icons/pa_kalorycznosc.png");
+const wet = require("../../assets/icons/pa_wilgotnosc.png");
+const ash = require("../../assets/icons/pa_popiol.png");
+const sulfur = require("../../assets/icons/pa_siarka.png");
+
+export const Attributes = ({ product }) => {
+  return (
+    <AttributesContainer>
+      {product.Kalorycznosc && (
+        <Attribute>
+          <img src={calc} />
+          {product.Kalorycznosc}
+        </Attribute>
+      )}
+      {product.Wilgoc && (
+        <Attribute>
+          <img src={wet} />
+          {product.Wilgoc}
+        </Attribute>
+      )}
+      {product.Popiol && (
+        <Attribute>
+          <img src={ash} />
+          {product.Popiol}
+        </Attribute>
+      )}
+      {product.Siarka && (
+        <Attribute>
+          <img src={sulfur} />
+          {product.Siarka}
+        </Attribute>
+      )}
+    </AttributesContainer>
+  );
+};
+
+const Attribute = styled.div`
+  display: flex;
+  flex-flow: column;
+  text-align: center;
+
+  > img {
+    width: 3rem;
+    height: 3rem;
+    margin: 1rem auto;
+  }
+`;
+
+const AttributesContainer = styled.div`
+  display: flex;
+  flex-flow: row;
+  justify-content: space-around;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid black;
+  background: ${({ theme }) => theme.colors.background};
+
+  @media ${device.tablet} {
+    width: 85%;
+    left: 5%;
+    height: 200px;
+  }
+`;
--- src/templates/product.tsx
+++ src/templates/product.tsx
@@ -7,14 +7,12 @@ import { TransportSection } from "../components/oragnisms/transportSection";
 import { OldPrice } from "../components/atoms/oldPrice";
 import { device } from "../styles/breakpoints";
 const polygon = require("../assets/Polygon.svg");
-const calc = require("../assets/icons/pa_kalorycznosc.png");
-const wet = require("../assets/icons/pa_wilgotnosc.png");
-const ash = require("../assets/icons/pa_popiol.png");
-const sulfur = require("../assets/icons/pa_siarka.png");
+
 import ReactMarkdown from "react-markdown";
 import { RecommendedProducts } from "../components/oragnisms/recommendedProducts";
 import { HeadingSection } from "../components/atoms/headingSection";
 import { StyledWhitespace } from "../components/atoms/whitespace";
+import { Attributes } from "../components/molecules/attributes";
 
 export const ProductTemplate = ({ data }) => {
   const product = data.allStrapiEkogroszeks.edges[0].node;
@@ -24,33 +22,9 @@ export const ProductTemplate = ({ data }) => {
       <StyledContainer>
         <ImageWrapper>
           <img src={`${process.env.IMAGE_URL}${product.Grafika[0].url}`} />
-          <AttributesContainer>
-            {product.Kalorycznosc && (
-              <Attribute>
-                <img src={calc} />
-                {product.Kalorycznosc}
-              </Attribute>
-            )}
-            {product.Wilgoc && (
-              <Attribute>
-                <img src={wet} />
-                {product.Wilgoc}
-              </Attribute>
-            )}
-            {product.Popiol && (
-              <Attribute>
-                <img src={ash} />
-                {product.Popiol}
-              </Attribute>
-            )}
-            {product.Siarka && (
-              <Attribute>
-                <img src={sulfur} />
-                {product.Siarka}
-              </Attribute>
-            )}
-          </AttributesContainer>
+          <Attributes product={product} />
         </ImageWrapper>
+        
         <ContentContainer>
           <Title>{product.Nazwa}</Title>
           <Price>{product.AkutalnaCena.toFixed(2)}zł</Price>
@@ -77,36 +51,6 @@ export const ProductTemplate = ({ data }) => {
 };
 
 export default ProductTemplate;
-
-const Attribute = styled.div`
-  display: flex;
-  flex-flow: column;
-  text-align: center;
-
-  > img {
-    width: 3rem;
-    height: 3rem;
-    margin: 1rem auto;
-  }
-`;
-
-const AttributesContainer = styled.div`
-  display: flex;
-  flex-flow: row;
-  justify-content: space-around;
-  position: absolute;
-  bottom: 0;
-  width: 100%;
-  height: 100px;
-  border-top: 1px solid black;
-  background: ${({ theme }) => theme.colors.background};
-
-  @media ${device.tablet} {
-    width: 85%;
-    left: 5%;
-    height: 200px;
-  }
-`;
 
 const ImageWrapper = styled.div`
   position: relative;
