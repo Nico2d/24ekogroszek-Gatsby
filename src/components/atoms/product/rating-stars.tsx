7c268296b77331d8c11664290f61c21ed640e2a7
Nico <nico30d@gmail.com>
2021-03-12 10:34:44 +0100

    fix: error of passing atribiute to icon

--- src/components/atoms/product/rating-stars.tsx
+++ src/components/atoms/product/rating-stars.tsx
@@ -16,31 +16,32 @@ export const RatingStars: React.FC<RagingStarsProps> = ({
   let Stars = [];
   for (let i = 1; i <= maxRate; i++) {
     Stars.push(
-      <StyledLabel key={i}>
-        <StyledStars isActive={i <= rate} />
-        <input
+      <StyledStars key={i} isActive={i <= rate}>
+        <AiFillStar />
+        <HiddenInput
           value={i}
           ref={register}
           name="rate"
           type="radio"
           onChange={(e) => setRate(+e.target.value)}
         />
-      </StyledLabel>
+      </StyledStars>
     );
   }
 
   return <div>{Stars}</div>;
 };
 
-const StyledLabel = styled.label`
-  input {
-    display: none;
-  }
+const HiddenInput = styled.input`
+  display: none;
 `;
 
-const StyledStars = styled(AiFillStar)<{ isActive: boolean }>`
-  fill: ${({ isActive }) => (isActive ? "#f2994a" : "#666666")};
+const StyledStars = styled.label<{ isActive: boolean }>`
   cursor: pointer;
-  width: 1.5rem;
-  height: 1.5rem;
+
+  > svg {
+    fill: ${({ isActive }) => (isActive ? "#f2994a" : "#666666")};
+    width: 1.5rem;
+    height: 1.5rem;
+  }
 `;
