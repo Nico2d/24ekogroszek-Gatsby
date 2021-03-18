c002d10d3ba892b3d92fc13a5f70a2f861f2783d
Nico <nico30d@gmail.com>
2021-03-18 20:51:55 +0100

    fix: problem with build becouse of leaflet

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,5 +25,11 @@ module.exports = {
         queryLimit: 1000,
       },
     },
+    {
+      resolve: "gatsby-plugin-react-leaflet",
+      options: {
+        linkStyles: true, // (default: true) Enable/disable loading stylesheets via CDN
+      },
+    },
   ],
 };
