02865073f6fc9dc59e1943beef6823ca0fbba655
Nico <nico30d@gmail.com>
2021-03-22 08:51:53 +0100

    fix: change font color to theme provider

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -2,7 +2,8 @@ import { DefaultTheme } from "styled-components";
 
 export const theme: DefaultTheme = {
   colors: {
-    primary: "linear-gradient(to right, #F2994A, #EB5757, #EB5757)",
+    primary:
+      "linear-gradient(to right top, #27ae60, #45be57, #64ce4a, #85dd36, #a8eb12)", //"linear-gradient(to right, #F2994A, #EB5757, #EB5757)",
     secondary: "#E5E5E5",
     cardColor: "#FFFFFF",
     background: "#E5E5E5",
